8c1b809b2b0006d8a9fc699767ee9a5efd108387
Brian Longmore <branodev@gmail.com>
2022-02-19 23:10:28 -1000

    add unit tests for added functions

--- funcs_test.go
+++ funcs_test.go
@@ -52,3 +52,107 @@ func TestGetWordByDate(t *testing.T) {
 		})
 	}
 }
+
+func TestGetWordByString(t *testing.T) {
+	type args struct {
+		dateString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "'dodge' feb 18, 2022",
+			args: args{
+				dateString: "2022-02-18",
+			},
+			want:    "dodge",
+			wantErr: false,
+		},
+		{
+			name: "'shake' feb 17, 2022",
+			args: args{
+				dateString: "2022-02-17",
+			},
+			want:    "shake",
+			wantErr: false,
+		},
+		{
+			name: "out of date range",
+			args: args{
+				dateString: "1950-01-01",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetWordByString(tt.args.dateString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWordByString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetWordByString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetWordByInteger(t *testing.T) {
+	type args struct {
+		year  int
+		month int
+		day   int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "'dodge' feb 18, 2022",
+			args: args{
+				month: 2,
+				day:   18,
+				year:  2022,
+			},
+			want:    "dodge",
+			wantErr: false,
+		},
+		{
+			name: "'shake' feb 17, 2022",
+			args: args{
+				month: 2,
+				day:   17,
+				year:  2022,
+			},
+			want:    "shake",
+			wantErr: false,
+		},
+		{
+			name: "out of date range",
+			args: args{
+				month: 1,
+				day:   1,
+				year:  1950,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetWordByInteger(tt.args.year, tt.args.month, tt.args.day)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWordByInteger() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetWordByInteger() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
