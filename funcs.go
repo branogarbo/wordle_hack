c53c564af68cb9e6ce23202cdb13d2666d465833
Brian Longmore <branodev@gmail.com>
2022-02-19 23:56:34 -1000

    keep wordle data in memory

--- funcs.go
+++ funcs.go
@@ -1,19 +1,12 @@
 package wordle_hack
 
 import (
-	"encoding/json"
 	"errors"
-	"os"
 	"time"
 )
 
 func GetWordByDate(date time.Time) (string, error) {
 	date = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.Now().UTC().Location())
-
-	words, err := readJSON("./data/word_list_ordered.json")
-	if err != nil {
-		return "", err
-	}
 
 	wdm := popWordDateMap(words)
 	word := wdm[date]
@@ -26,18 +19,12 @@ func GetWordByDate(date time.Time) (string, error) {
 }
 
 func GetWordByString(dateString string) (string, error) {
-	words, err := readJSON("./data/word_list_ordered.json")
-	if err != nil {
-		return "", err
-	}
-
-	wdm := popWordDateMap(words)
-
 	date, err := time.Parse("2006-01-02T15:04:05.000Z", dateString+"T00:00:00.000Z")
 	if err != nil {
 		return "", err
 	}
 
+	wdm := popWordDateMap(words)
 	word := wdm[date]
 
 	if word == "" {
@@ -48,18 +35,9 @@ func GetWordByString(dateString string) (string, error) {
 }
 
 func GetWordByInteger(year, month, day int) (string, error) {
-	words, err := readJSON("./data/word_list_ordered.json")
-	if err != nil {
-		return "", err
-	}
+	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Now().UTC().Location())
 
 	wdm := popWordDateMap(words)
-
-	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Now().UTC().Location())
-	if err != nil {
-		return "", err
-	}
-
 	word := wdm[date]
 
 	if word == "" {
@@ -93,20 +71,4 @@ func popWordDateMap(words Words) WordDateMap {
 	}
 
 	return word_date_map
-}
-
-func readJSON(path string) (Words, error) {
-	var wordsOut Words
-
-	byteOut, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(byteOut, &wordsOut)
-	if err != nil {
-		return nil, err
-	}
-
-	return wordsOut, nil
 }
